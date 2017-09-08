c89cb015fb37677f310dd588c6b24f8e924e1408
Fernando Queirolo <queirolo.fer@gmail.com>
2017-09-08 19:57:20 -0300

    ROJO: Se agrega el metodo para transferir un monto de una cuenta a otra

--- src/test/CuentasTest.java
+++ src/test/CuentasTest.java
@@ -16,9 +16,20 @@ public class CuentasTest {
 
 	@Test
 	// Verifica que se sume un monto determinado a una cuenta;
-	public void SumarMonto() { 
+	public void SumarMonto() {
 		Cuenta cuentaA = new Cuenta();
 		cuentaA.SumarMonto(40);
 		Assert.assertTrue(cuentaA.Estado() == 40);
 	}
+
+	@Test
+	// Verifica que se transfiera un monto de una cuenta a otra.
+	public void TransferirMontoA() {
+		Cuenta cuentaA = new Cuenta();
+		Cuenta cuentaB = new Cuenta();
+		cuentaA.SumarMonto(100);
+		cuantaA.TransferirMontoA(cuentaB);
+		Assert.assertTrue(cuentaB.Estado() == 100 && cuentaA.Estado() == 0);
+	}
+
 }

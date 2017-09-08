684eba1d1f92961c5eadbdab0f534eee8c78fc1c
Fernando Queirolo <queirolo.fer@gmail.com>
2017-09-08 19:53:26 -0300

    ROJO: Se agrega el metodo para Incrementar el monto de una cuenta

--- src/test/CuentasTest.java
+++ src/test/CuentasTest.java
@@ -6,10 +6,19 @@ import org.junit.Test;
 import cuentaBancaria.Cuenta;
 
 public class CuentasTest {
-	
-		@Test
-		public void AbreCuenta(){
-			Cuenta account = new Cuenta();
-			Assert.assertTrue(account.Estado() == 0);
-		}
+
+	@Test
+	// Verifica que abra una nueva cuenta con monto en 0
+	public void AbreCuenta() {
+		Cuenta account = new Cuenta();
+		Assert.assertTrue(account.Estado() == 0);
+	}
+
+	@Test
+	// Verifica que se sume un monto determinado a una cuenta;
+	public void SumarMonto() { 
+		Cuenta cuentaA = new Cuenta();
+		cuentaA.sumarMonto(40);
+		Assert.assertTrue(cuentaA.Estado() == 40);
+	}
 }
